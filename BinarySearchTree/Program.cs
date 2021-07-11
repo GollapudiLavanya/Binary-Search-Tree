83c899c14a7bc05b3cc22a96698e650cc4aca46e
GollapudiLavanya <lavanyagollapudi5166@gmail.com>
2021-07-11 21:59:01 +0530

    [LAVANYA]Initial Commit

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -7,6 +7,11 @@ namespace BinarySearchTree
         static void Main(string[] args)
         {
             Console.WriteLine("-----------------Binary Search Tree--------------");
+            BinaryTree<int> binaryTree = new BinaryTree<int>(56);
+
+            binaryTree.Insert(30);
+            binaryTree.Insert(70);
+            binaryTree.Display();
         }
     }
 }
